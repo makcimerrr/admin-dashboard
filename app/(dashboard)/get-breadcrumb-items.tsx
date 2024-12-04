9a76b8b55ccc8f64399efe81b2c9f384b327e216
makcimerrr <maximedubs@proton.me>
2024-12-04 17:57:37 +0100

    feat: create a dynamic breadcrumb items component for the website

---
+++ app/(dashboard)/get-breadcrumb-items.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import React from 'react';
+
+export function DashboardBreadcrumb() {
+  const pathname = usePathname(); // Récupérer le chemin actuel
+
+  // Mapping des chemins vers des titres
+  const pathMap: { [key: string]: string } = {
+    '/': 'Dashboard',
+    '/students': 'Students',
+    '/customers': 'Customers',
+    '/orders': 'Orders',
+    '/settings': 'Settings',
+  };
+
+  // Divise le chemin en segments
+  const pathSegments = pathname.split('/').filter(Boolean);
+  const breadcrumbItems = pathSegments.map((segment, index) => {
+    const path = '/' + pathSegments.slice(0, index + 1).join('/');
+    return {
+      title: pathMap[path] || segment,
+      href: path,
+      isCurrent: index === pathSegments.length - 1,
+    };
+  });
+
+  return (
+    <Breadcrumb className="hidden md:flex">
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link href="/">Dashboard</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        {breadcrumbItems.map((item) => (
+          <React.Fragment key={item.href}>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              {item.isCurrent ? (
+                <BreadcrumbPage>{item.title}</BreadcrumbPage>
+              ) : (
+                <BreadcrumbLink asChild>
+                  <Link href={item.href}>{item.title}</Link>
+                </BreadcrumbLink>
+              )}
+            </BreadcrumbItem>
+          </React.Fragment>
+        ))}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
