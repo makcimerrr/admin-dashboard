078c1b2b30d14834c8b5286bab2d202a3aee685c
makcimerrr <maximedubs@proton.me>
2025-01-10 13:57:08 +0100

    feat(management): add manager for holidays and projects

--- app/(dashboard)/promos/page.tsx
+++ app/(dashboard)/promos/page.tsx
@@ -1,10 +1,16 @@
 import PromoManager from '../promo-management';
+import HolidayManager from '../holiday-management';
+import ProjectManager from '../project-management';
 
 export default function PromoPage() {
   return (
     <div className="p-6">
       <h1 className="text-xl font-bold mb-4">Gestion des Promotions</h1>
       <PromoManager />
+      <h1 className="text-xl font-bold mb-4">Gestion des Vacances</h1>
+      <HolidayManager />
+      <h1 className="text-xl font-bold mb-4">Gestion des Projets</h1>
+      <ProjectManager />
     </div>
   );
 }
