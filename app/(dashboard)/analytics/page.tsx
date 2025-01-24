e840624ff8cc335f9c7f64d38b81808b783a41c0
makcimerrr <maximedubs@proton.me>
2025-01-24 13:55:46 +0100

    feat(chart): refactor and adding one more chart component and added at page analytics

--- app/(dashboard)/analytics/page.tsx
+++ app/(dashboard)/analytics/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import ChartStudent from '@/components/chart-student';
+import PieChart from '@/components/pie-chart-student';
+import BarChart from '@/components/bar-chart-student';
 
 const AnalyticsPage: React.FC = () => {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-semibold text-gray-900 mb-4">Analytics</h1>
-      <ChartStudent />
+      <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-2">
+        <PieChart />
+        <BarChart />
+      </div>
     </div>
   );
 };
