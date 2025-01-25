9868624180ba3616965a192c38ee82da13d65639
makcimerrr <maximedubs@proton.me>
2025-01-25 02:47:00 +0100

    feat(analytics): remove casual charts and adding map for create one chart for each promo

--- app/(dashboard)/analytics/page.tsx
+++ app/(dashboard)/analytics/page.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import PieChart from '@/components/pie-chart-student';
 import BarChart from '@/components/bar-chart-student';
+import promos from 'config/promoConfig.json' assert { type: 'json' };
 
 const AnalyticsPage: React.FC = () => {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-semibold text-gray-900 mb-4">Analytics</h1>
       <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-2">
-        <PieChart />
-        <BarChart />
+        {promos.map((promo) => (
+          <PieChart title={promo.title} eventID={promo.eventId} keyPromo={promo.key} />
+        ))}
       </div>
     </div>
   );
