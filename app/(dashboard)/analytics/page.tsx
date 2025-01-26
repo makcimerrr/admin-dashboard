9361c4849acca04b1138790099aad4f7e45e5da9
makcimerrr <maximedubs@proton.me>
2025-01-26 02:20:32 +0100

    feat(analytics): add one more chart for each promo, that track the number of student per promo whose progress status is late and good per month

--- app/(dashboard)/analytics/page.tsx
+++ app/(dashboard)/analytics/page.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import PieChart from '@/components/pie-chart-student';
-import BarChart from '@/components/bar-chart-student';
+import BarChartStacked from '@/components/bar-chart-student-stacked';
 import promos from 'config/promoConfig.json' assert { type: 'json' };
 
 export default function AnalyticsPage() {
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-semibold text-gray-900 mb-4">Analytics</h1>
-      <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-2">
+    <div className="p-6 bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold text-gray-900 mb-6">Analytics</h1>
+      <div className="grid gap-6 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
         {promos.map((promo) => (
           <PieChart
+            title={promo.title}
+            eventID={promo.eventId}
+            keyPromo={promo.key}
+          />
+        ))}
+      </div>
+      <div className="mt-8 grid gap-6 sm:grid-cols-1 lg:grid-cols-2">
+        {promos.map((promo) => (
+          <BarChartStacked
             title={promo.title}
             eventID={promo.eventId}
             keyPromo={promo.key}
