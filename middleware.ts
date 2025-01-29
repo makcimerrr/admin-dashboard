0a5f3e9eb7618b327e755a1dd2f6ad8e8b28fb33
makcimerrr <maximedubs@proton.me>
2025-01-29 21:05:36 +0100

    feat(middleware): add protection if not session

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,20 @@
-export { auth as middleware } from '@/lib/auth';
+// middleware.ts
+import { NextResponse } from 'next/server';
+import { auth } from '@/lib/auth';
+
+export async function middleware(request: { url: string | URL | undefined }) {
+  const session = await auth();
+  const user = session?.user;
+
+  const loginUrl = new URL('/login', request.url);
+
+  // Avoid redirect loop by checking if the request is already for the login page
+  if (!user && request.url !== loginUrl.toString()) {
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
 
 // Don't invoke Middleware on some paths
 export const config = {
