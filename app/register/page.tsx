4c6df69d064c626b253db1cc0bcf02b4c4fbac75
makcimerrr <maximedubs@proton.me>
2025-01-29 21:04:39 +0100

    feat(page): create login and register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,8 +1,7 @@
 import { GalleryVerticalEnd } from "lucide-react"
+import { RegisterForm } from "@/components/register-form"
 
-import { LoginForm } from "@/components/login-form"
-
-export default function LoginPage() {
+export default function RegisterPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
@@ -12,7 +11,7 @@ export default function LoginPage() {
           </div>
           Zone01 Rouen
         </a>
-        <LoginForm />
+        <RegisterForm />
       </div>
     </div>
   )
