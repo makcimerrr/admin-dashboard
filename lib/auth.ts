15640b675220caa61949d65d60b9c7edb439cf4a
makcimerrr <maximedubs@proton.me>
2025-01-30 10:38:51 +0100

    feat(auth): update provider to use api link to db for credentials auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,31 +1,44 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
-import Google from 'next-auth/providers/google';
-import Credentials from 'next-auth/providers/credentials';
+import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GitHub,
     Google,
     Credentials({
-      name: 'Credentials',
+      name: "Credentials",
       credentials: {
-        email: {
-          label: 'Email',
-          type: 'email',
-          placeholder: 'jsmith@example.com'
-        },
-        password: { label: 'Password', type: 'password' }
+        email: { label: "Email", type: "email", placeholder: "jsmith@example.com" },
+        password: { label: "Password", type: "password" }
       },
-      async authorize(credentials) {
-        // Remplace cette logique par une vérification réelle (ex: via une base de données)
-        if (
-          credentials.email === 'test@example.com' &&
-          credentials.password === 'password'
-        ) {
-          return { id: '1', name: 'John Doe', email: 'test@example.com' };
+      async authorize(credentials, req) {
+        if (!credentials) {
+          throw new Error('No credentials provided');
         }
-        throw new Error('Invalid email or password');
+
+        // Récupérer l'origine de la requête
+        const host = req.headers.get("host"); // Récupère l'hôte depuis les headers
+        const protocol = process.env.NEXTAUTH_URL?.startsWith("https") ? "https" : "http";
+        const baseUrl = host ? `${protocol}://${host}` : process.env.NEXTAUTH_URL;
+
+        const response = await fetch(`${baseUrl}/api/authenticate`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: credentials.email,
+            password: credentials.password
+          })
+        });
+
+        if (response.ok) {
+          return await response.json();
+        } else {
+          throw new Error('Invalid email or password');
+        }
       }
     })
   ],
@@ -48,9 +61,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }
   },
   session: {
-    strategy: 'jwt'
-  },
-  pages: {
-    signIn: '/login'
+    strategy: "jwt",
   }
 });
