d41f1090d448ec1fb8c7d523ef25ee28e9d5fa8e
makcimerrr <maximedubs@proton.me>
2025-01-30 11:20:08 +0100

    feat(auth): create function sign-in when login w/ github or google to save in db

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,17 +1,21 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
-import Google from "next-auth/providers/google";
-import Credentials from "next-auth/providers/credentials";
+import Google from 'next-auth/providers/google';
+import Credentials from 'next-auth/providers/credentials';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GitHub,
     Google,
     Credentials({
-      name: "Credentials",
+      name: 'Credentials',
       credentials: {
-        email: { label: "Email", type: "email", placeholder: "jsmith@example.com" },
-        password: { label: "Password", type: "password" }
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'jsmith@example.com'
+        },
+        password: { label: 'Password', type: 'password' }
       },
       async authorize(credentials, req) {
         if (!credentials) {
@@ -19,9 +23,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         }
 
         // Récupérer l'origine de la requête
-        const host = req.headers.get("host"); // Récupère l'hôte depuis les headers
-        const protocol = process.env.NEXTAUTH_URL?.startsWith("https") ? "https" : "http";
-        const baseUrl = host ? `${protocol}://${host}` : process.env.NEXTAUTH_URL;
+        const host = req.headers.get('host'); // Récupère l'hôte depuis les headers
+        const protocol = process.env.NEXTAUTH_URL?.startsWith('https')
+          ? 'https'
+          : 'http';
+        const baseUrl = host
+          ? `${protocol}://${host}`
+          : process.env.NEXTAUTH_URL;
 
         const response = await fetch(`${baseUrl}/api/authenticate`, {
           method: 'POST',
@@ -43,6 +51,34 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     })
   ],
   callbacks: {
+    async signIn({ user, account, profile }) {
+      if (account && (account.provider === 'github' || account.provider === 'google')) {
+        const email = user.email;
+        const name = user.name;
+
+        const host = process.env.VERCEL_URL;
+        const protocol = process.env.NEXTAUTH_URL?.startsWith('https') ? 'https' : 'http';
+        const baseUrl = host ? `${protocol}://${host}` : process.env.NEXTAUTH_URL;
+
+
+        const response = await fetch(`${baseUrl}/api/save-oauth-user`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: user.email,
+            name: user.name
+          })
+        });
+
+        if (response.ok) {
+          return true; // Return true to indicate successful sign-in
+        }
+        return false; // Return false to indicate failure
+      }
+      return false; // Return false if account is not github or google
+    },
     async jwt({ token, user }) {
       if (user) {
         token.id = user.id;
@@ -61,6 +97,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }
   },
   session: {
-    strategy: "jwt",
+    strategy: 'jwt'
   }
 });
