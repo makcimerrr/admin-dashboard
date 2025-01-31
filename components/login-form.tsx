8124c29c09e5ea55c232478c2e7043a643c7a2ff
makcimerrr <maximedubs@proton.me>
2025-01-31 17:09:22 +0100

    fix: edit texte error in login form

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -98,7 +98,7 @@ export function LoginForm({
         <CardHeader className="text-center">
           <CardTitle className="text-xl">Welcome back</CardTitle>
           <CardDescription>
-            Login with your Apple or Google account
+            Login with your Google or Github account
           </CardDescription>
         </CardHeader>
         <CardContent>
