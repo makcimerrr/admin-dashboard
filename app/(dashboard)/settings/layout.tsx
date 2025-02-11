eeec61de1b6047ecfffa369e5e8658291a0366cd
makcimerrr <maximedubs@proton.me>
2025-02-11 11:02:13 +0100

    feat(layout): add progress to load screen while loading data

--- app/(dashboard)/settings/layout.tsx
+++ app/(dashboard)/settings/layout.tsx
@@ -5,6 +5,8 @@ import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import { usePathname } from 'next/navigation';
 import * as React from 'react';
+import { Progress } from '@/components/ui/progress';
+import {useEffect, useState} from "react";
 
 const menuItems = [
   { name: 'Profile', href: '/settings' },
@@ -20,6 +22,29 @@ export default function SettingsLayout({
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
+  const [progress, setProgress] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setProgress(10);
+
+    let newProgress = 10;
+    const timer = setInterval(() => {
+      newProgress += Math.floor(Math.random() * (50 - 10 + 1)) + 15;
+      setProgress(newProgress);
+      if (newProgress >= 100) {
+        clearInterval(timer);
+        setTimeout(() => setLoading(false), 300);
+      }
+    }, 500);
+
+    return () => {
+      clearInterval(timer);
+      setProgress(0);
+    };
+  }, [pathname]);
+
   return (
     <div className="hidden space-y-6 p-10 pb-16 md:block">
       {/* Title */}
@@ -48,7 +73,13 @@ export default function SettingsLayout({
         </aside>
 
         {/* Main Content */}
-        <main className="flex-1 p-10">{children}</main>
+        <main className="flex-1 p-10">
+          {loading ? (
+            <Progress value={progress} className="w-[60%]" />
+          ) : (
+            children
+          )}
+        </main>
       </div>
     </div>
   );
