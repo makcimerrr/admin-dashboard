1e179ba4974730af02bcebf53a0596f8546891d1
makcimerrr <maximedubs@proton.me>
2025-02-15 16:32:24 +0100

    chore: remove error import module

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,16 +1,20 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 import path from "path";
-import fs from "fs";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 export async function getPromoStatus() {
+  if (typeof window !== 'undefined') {
+    throw new Error('getPromoStatus can only be used on the server side');
+  }
+
+  const fs = await import('fs').then(mod => mod.promises);
   const __dirname = path.dirname(new URL(import.meta.url).pathname);
   const rootDir = path.join(__dirname, '../'); // En remontant jusqu'à la racine du projet
   const jsonFilePath = path.join(rootDir, 'config', 'promoStatus.json'); // Chemin vers config/promoStatus.json
-  const fileContent = await fs.promises.readFile(jsonFilePath, 'utf-8');
+  const fileContent = await fs.readFile(jsonFilePath, 'utf-8');
   return JSON.parse(fileContent);
 }
