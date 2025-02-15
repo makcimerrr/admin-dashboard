0d41004cfacc721c7e64b6c60e5565ad109a9c34
makcimerrr <maximedubs@proton.me>
2025-02-15 16:24:50 +0100

    feat(api): create path update to start a cron job

--- app/api/update/route.ts
+++ app/api/update/route.ts
@@ -3,8 +3,10 @@ import path from "path";
 import fs from "fs";
 
 export async function getPromoStatus() {
-  const filePath = path.join(process.cwd(), 'config', 'promoStatus.json');
-  const fileContent = await fs.promises.readFile(filePath, 'utf-8');
+  const __dirname = path.dirname(new URL(import.meta.url).pathname);
+  const rootDir = path.join(__dirname, '../'); // En remontant jusqu'à la racine du projet
+  const jsonFilePath = path.join(rootDir, 'config', 'promoStatus.json'); // Chemin vers config/promoStatus.json
+  const fileContent = await fs.promises.readFile(jsonFilePath, 'utf-8');
   return JSON.parse(fileContent);
 }
 
