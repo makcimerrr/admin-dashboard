58cddb2d6e68a2d599fd8b96d2884abefd236826
makcimerrr <maximedubs@proton.me>
2025-03-19 08:49:02 +0100

    chore: install components and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,41 @@
-export default {
+import createMDX from '@next/mdx'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
+        protocol: 'https' as const,
         hostname: 'avatars.githubusercontent.com'
       },
       {
-        protocol: 'https',
+        protocol: 'https' as const,
         hostname: '*.public.blob.vercel-storage.com'
       },
       {
-        protocol: 'https',
+        protocol: 'https' as const,
         hostname: 'lh3.googleusercontent.com'
       }
     ]
-  }
-};
+  },
+  experimental: {
+    appDir: true,
+    mdxRs: true,
+  },
+}
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    // Si vous voulez utiliser remark et rehype plugins
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
