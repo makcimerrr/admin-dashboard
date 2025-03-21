eac5bfacf9c409560c83075f5c147c02aab9f859
makcimerrr <maximedubs@proton.me>
2025-03-21 16:52:11 +0100

    feat(app-sidebar): change data for navMain

--- app/(home)/app-sidebar.tsx
+++ app/(home)/app-sidebar.tsx
@@ -1,152 +1,177 @@
-"use client";
+'use client';
 
-import * as React from "react"
+import * as React from 'react';
 import {
-    AudioWaveform,
-    BookOpen,
-    Bot,
-    Command,
-    Frame,
-    GalleryVerticalEnd,
-    Map,
-    PieChart,
-    Settings2,
-    SquareTerminal,
-} from "lucide-react"
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Command,
+  Frame,
+  GalleryVerticalEnd,
+  Map,
+  PieChart,
+  Settings2,
+  SquareTerminal
+} from 'lucide-react';
 
-import { NavMain } from "./nav-main"
-import { NavProjects } from "./nav-projects"
-import { NavUser } from "./nav-user"
-import { TeamSwitcher } from "./team-switcher"
+import { NavMain } from './nav-main';
+import { NavProjects } from './nav-projects';
+import { NavUser } from './nav-user';
+import { TeamSwitcher } from './team-switcher';
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem,
-    SidebarRail,
-} from "@/components/ui/sidebar"
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail
+} from '@/components/ui/sidebar';
 
 // This is sample data.
 const data = {
-    navMain: [
+  navMain: [
+    {
+      title: 'Models',
+      url: '#',
+      icon: Bot,
+      items: [
         {
-            title: "Playground",
-            url: "#",
-            icon: SquareTerminal,
-            isActive: true,
-            items: [
-                {
-                    title: "History",
-                    url: "#",
-                },
-                {
-                    title: "Starred",
-                    url: "#",
-                },
-                {
-                    title: "Settings",
-                    url: "#",
-                },
-            ],
+          title: 'Phi3',
+          url: '#'
         },
         {
-            title: "Models",
-            url: "#",
-            icon: Bot,
-            items: [
-                {
-                    title: "Genesis",
-                    url: "#",
-                },
-                {
-                    title: "Explorer",
-                    url: "#",
-                },
-                {
-                    title: "Quantum",
-                    url: "#",
-                },
-            ],
+          title: 'Llama3.2',
+          url: '#'
         },
         {
-            title: "Documentation",
-            url: "#",
-            icon: BookOpen,
-            items: [
-                {
-                    title: "Introduction",
-                    url: "#",
-                },
-                {
-                    title: "Get Started",
-                    url: "#",
-                },
-                {
-                    title: "Tutorials",
-                    url: "#",
-                },
-                {
-                    title: "Changelog",
-                    url: "#",
-                },
-            ],
+          title: 'Deepseek-r1',
+          url: '#'
         },
         {
-            title: "Settings",
-            url: "#",
-            icon: Settings2,
-            items: [
-                {
-                    title: "General",
-                    url: "/settings",
-                },
-                {
-                    title: "Team",
-                    url: "#",
-                },
-                {
-                    title: "Billing",
-                    url: "#",
-                },
-                {
-                    title: "Limits",
-                    url: "#",
-                },
-            ],
+          title: 'DeepSeek-LLM',
+          url: '#'
+        }
+      ]
+    },
+    {
+      title: 'Documentation',
+      url: '/docs',
+      icon: BookOpen,
+      isActive: true,
+      items: [
+        {
+          title: 'Introduction',
+          url: '/docs/introduction'
         },
-    ],
-    projects: [
-    ],
-}
+        {
+          title: 'Authentication',
+          url: '/docs/authentication'
+        },
+        {
+          title: 'Endpoints',
+          url: '#'
+        },
+        {
+          title: 'SDKs & Integrations',
+          url: '#'
+        },
+        {
+          title: 'Exemples',
+          url: '#'
+        },
+        {
+          title: 'FAQ',
+          url: '#'
+        }
+      ]
+    },
+    {
+      title: 'Ressources',
+      url: '#',
+      icon: Frame,
+      items: [
+        {
+          title: 'Changelog',
+          url: '#'
+        },
+        {
+          title: "Status de l'API",
+          url: '#'
+        },
+        {
+          title: 'Support',
+          url: '#'
+        }
+      ]
+    },
+    {
+      title: 'Settings',
+      url: '#',
+      icon: Settings2,
+      items: [
+        {
+          title: 'General',
+          url: '/settings'
+        },
+        {
+          title: 'Account',
+          url: '#'
+        },
+        {
+          title: 'Notifications',
+          url: '#'
+        },
+        {
+          title: 'Security',
+          url: '#'
+        },
+        {
+          title: 'API Keys',
+          url: '#'
+        }
+      ]
+    }
+  ],
+  projects: []
+};
 
-export function AppSidebar({user, logout, ...props}: { user: any, logout: any} & React.ComponentProps<typeof Sidebar>) {
-    return (
-        <Sidebar collapsible="icon" {...props}>
-            <SidebarHeader>
-                <SidebarMenu>
-                    <SidebarMenuItem>
-                        <SidebarMenuButton size="lg" asChild>
-                            <a href="/">
-                                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                                    <GalleryVerticalEnd className="size-4" />
-                                </div>
-                                <div className="flex flex-col gap-0.5 leading-none">
-                                    <span className="font-semibold">Zone01 Dashboard</span>
-                                    <span className="">v1.0.0</span>
-                                </div>
-                            </a>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
-                </SidebarMenu>
-            </SidebarHeader>
-            <SidebarContent>
-                <NavMain items={data.navMain}/>
-                {/*<NavProjects projects={data.projects}/>*/}
-            </SidebarContent>
-            <SidebarFooter>
-                <NavUser user={user} logout={logout}/>
-            </SidebarFooter>
-            <SidebarRail/>
-        </Sidebar>
-    )
+export function AppSidebar({
+  user,
+  logout,
+  ...props
+}: {
+  user: any;
+  logout: any;
+} & React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="/">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <GalleryVerticalEnd className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-semibold">Zone01 Dashboard</span>
+                  <span className="">v1.0.0</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        {/*<NavProjects projects={data.projects}/>*/}
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={user} logout={logout} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
 }
