cd6e1927c6a0068b054a2f1211dce64e69699ee0
makcimerrr <maximedubs@proton.me>
2025-07-24 17:00:24 +0200

    refactor: new sidebar from rebase better-front

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,23 +5,25 @@ import { Toaster } from '@/components/ui/toaster';
 import ThemeProviderWrapper from '@/components/theme-provider-wrapper';
 
 export const metadata = {
-  title: 'Zone01 Admin Dashboard',
-  description:
-    'A user admin dashboard configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, and Prettier.'
+    title: 'Zone01 Admin Dashboard',
+    description:
+        'A user admin dashboard configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, and Prettier.'
 };
 
 export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode;
+                                       children
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className="flex min-h-screen w-full flex-col">
-        <ThemeProviderWrapper>{children}</ThemeProviderWrapper>
-        <Toaster />
-      </body>
-      <Analytics />
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body className="flex min-h-screen w-full flex-col">
+        <ThemeProviderWrapper>
+            {children}
+            <Toaster />
+        </ThemeProviderWrapper>
+        </body>
+        <Analytics />
+        </html>
+    );
 }
