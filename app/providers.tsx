8fe3261818ffb5ad4bbdac52a034fb276e394b03
makcimerrr <maximedubs@proton.me>
2026-01-23 09:08:51 +0100

    feat: wrap providers with NextAuthProvider for authentication support

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,15 +3,18 @@
 import { StackProvider, StackTheme } from "@stackframe/stack";
 import { stackClientApp } from "@/lib/stack-client";
 import ThemeProviderWrapper from '@/components/theme-provider-wrapper';
+import { NextAuthProvider } from "@/components/providers/nextauth-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <StackProvider app={stackClientApp}>
-      <StackTheme>
-        <ThemeProviderWrapper>
-          {children}
-        </ThemeProviderWrapper>
-      </StackTheme>
-    </StackProvider>
+    <NextAuthProvider>
+      <StackProvider app={stackClientApp}>
+        <StackTheme>
+          <ThemeProviderWrapper>
+            {children}
+          </ThemeProviderWrapper>
+        </StackTheme>
+      </StackProvider>
+    </NextAuthProvider>
   );
 }
